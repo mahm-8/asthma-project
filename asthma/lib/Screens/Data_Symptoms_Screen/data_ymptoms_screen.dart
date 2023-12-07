47a229d0caff3cbaff490f72f721e7a559d7e765
wadha <143879896+Wadha21@users.noreply.github.com>
2023-12-07 21:36:06 +0300

    edit on screens

--- asthma/lib/Screens/Data_Symptoms_Screen/data_ymptoms_screen.dart
+++ asthma/lib/Screens/Data_Symptoms_Screen/data_ymptoms_screen.dart
@@ -1,36 +1,78 @@
-import 'package:asthma/Screens/HomeScreen/home_screen.dart';
-import 'package:asthma/Screens/HomeScreen/widgets/medication_reminder.dart';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
-class DataSymptomsScreen extends StatelessWidget {
+class DataSymptomsScreen extends StatefulWidget {
   DataSymptomsScreen({super.key});
+
+  @override
+  State<DataSymptomsScreen> createState() => _DataSymptomsScreenState();
+}
+
+class _DataSymptomsScreenState extends State<DataSymptomsScreen> {
   ScreenshotController screenshotController = ScreenshotController();
+
+  int counter = 0;
+  List symptoms = [];
+  Uint8List? imageFile;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Screenshot(
       controller: screenshotController,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('sssssss'),
-          Text('sssssss'),
-          Text('sssssss'),
-          Text('sssssss'),
+          Text("This text will be captured as image"),
           ElevatedButton(
-              onPressed: () {
-                screenshotController.captureFromWidget(HomeScreen());
+              onPressed: () async {
+                var container = ListView.builder(
+                  itemCount: symptoms.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('name'),
+                      subtitle: Text('description'),
+                      trailing: Text('Low'),
+                    );
+                  },
+                );
                 screenshotController
-                    .captureFromWidget(HomeScreen(),
-                        delay: const Duration(seconds: 1))
-                    .then((value) {
-                  final image = value;
-                  print(image);
+                    .captureFromWidget(
+                        InheritedTheme.captureAll(
+                            context, Material(child: container)),
+                        delay: Duration(seconds: 1))
+                    .then((capturedImage) {
+                  ShowCapturedWidget(context, capturedImage);
+                  _saved(capturedImage);
                 });
               },
               child: const Text("Capture")),
         ],
       ),
     ));
+  }
+
+  Future<dynamic> ShowCapturedWidget(
+      BuildContext context, Uint8List capturedImage) {
+    return showDialog(
+      useSafeArea: false,
+      context: context,
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: Text("Captured widget screenshot"),
+        ),
+        body: Center(
+            child: capturedImage != null
+                ? Image.memory(capturedImage)
+                : Container()),
+      ),
+    );
+  }
+
+  _saved(image) async {
+    final result = await ImageGallerySaver.saveImage(image);
+    print("File Saved to Gallery");
   }
 }
