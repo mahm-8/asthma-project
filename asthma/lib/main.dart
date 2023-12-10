46042c9822bf6e7653c956d3e1d9310b477e01db
Ruba Hilal <developerruba@outlook.com>
2023-12-10 19:41:56 +0300

    - add animation on lottie when audio plays

--- asthma/lib/main.dart
+++ asthma/lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:asthma/Screens/breathing/breathing_screen.dart';
 import 'package:asthma/Screens/loading/loading_screen.dart';
 import 'package:asthma/Screens/HomeScreen/home_screen.dart';
 import 'package:asthma/blocs/asthma_bloc/asthma_bloc.dart';
