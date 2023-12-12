f9fa2b8dd5e905b9f6e31645ae636158ee11f1c3
wadha <143879896+Wadha21@users.noreply.github.com>
2023-12-12 18:58:24 +0300

    dashboard

---
+++ dashbord/lib/screens/dashboard/widgets/information_card_widgets.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class InformationCard extends StatelessWidget {
+  InformationCard({
+    super.key,
+  });
+  TextEditingController taskController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: 300,
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 141, 181, 199),
+          borderRadius: BorderRadius.only(
+              // topLeft: Radius.circular(20),
+              bottomLeft: Radius.circular(20))),
+      child: Expanded(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'lib/assets/Dashboard-bro.png',
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Wadha Almutairi',
+                style: TextStyle(fontSize: 16),
+              ),
+              const Text(
+                'IT',
+                style: TextStyle(fontSize: 16),
+              ),
+              const Text(
+                'Location: Riyadh',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                'Tour Task Today',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Container(
+                height: 1,
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(4)),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InkWell(
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            content: Column(
+                              children: [
+                                Text('add new task '),
+                                TextField(
+                                  controller: taskController,
+                                )
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {}, child: Text('add task'))
+                            ],
+                          ));
+                },
+                child: Container(
+                  height: 40,
+                  width: 120,
+                  child: Row(children: [
+                    Icon(
+                      Icons.add,
+                      size: 25,
+                    ),
+                    Text('Create New')
+                  ]),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(30)),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
